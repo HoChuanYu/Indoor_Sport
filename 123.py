0bce496110a8d9f1b42cad993b80c3bedcfa18a5
HoChuanYu <44057809+HoChuanYu@users.noreply.github.com>
2022-03-05 16:35:57 +0800

    Delete 123.py

--- 123.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-cap = cv2.VideoCapture('Videos/video1.mp4')
-
-while True:
-    success, img = cap.read()
-    if cv2.waitKey(1) & 0xFF == ord('q') or not success:
-        break
-    cv2.imshow("Video", img)
